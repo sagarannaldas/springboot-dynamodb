62292af6e6d3c8d779ab3e6559f0224dfc0e7ed5
sagarannaldas <annal7057@gmail.com>
2026-04-14 22:31:22 +0530

    1. added aws java sdk

---
+++ src/main/java/com/sagarannaldas/entity/Address.java
@@ -0,0 +1,23 @@
+package com.sagarannaldas.entity;
+
+import java.io.Serializable;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@DynamoDBDocument
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address implements Serializable {
+	@DynamoDBAttribute
+	private String city;
+	@DynamoDBAttribute
+	private String state;
+	@DynamoDBAttribute
+	private long pincode;
+}
